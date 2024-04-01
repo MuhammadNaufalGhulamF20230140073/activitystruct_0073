7fef04f868164c84bbc6dcaddb335a8b878f82e9
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-04-01 15:46:23 +0700

    Membuat Input Nim Mahasiswa

--- StrukturC.cpp
+++ StrukturC.cpp
@@ -18,4 +18,6 @@ struct Mahasiswa
 int main(){
      Mahasiswa mhs[3];
     for ( int i=0;i<3;i++){
-        cout << "Data ke-"<<(i+1)<<(":")<<endl;
+    cout << "Data ke-"<<(i+1)<<(":")<<endl;
+    cout << "Nomor Mahasiswa :" ;
+    getline(cin, mhs[i].nim);
