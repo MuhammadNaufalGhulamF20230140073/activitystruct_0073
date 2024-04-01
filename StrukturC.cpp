f29d530b8d0d3b9d48ab2cc3d5bdba523f9041a6
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-04-01 15:48:39 +0700

    Membuat Input Alamat Mahasiswa ( Nama Kota)

--- StrukturC.cpp
+++ StrukturC.cpp
@@ -23,7 +23,9 @@ int main(){
     getline(cin, mhs[i].nim);
     cout << "Nama Mahasiswa :" ;
     getline(cin, mhs[i].nama);
-    
+
     cout << "Alamat Mahasiswa :" << endl;
     cout <<"\t Nama Desa : ";
     cin >> mhs[i].alamat.desa;
+    cout <<"\t Nama Kota : ";
+    cin >> mhs[i].alamat.kota;
